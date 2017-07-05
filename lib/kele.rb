af8d87f2c97783e7a3e9934f4afd17b389daf82d
Logan Tran <loganmartintran@icloud.com>
2017-07-05 16:16:22 -0500

    fix error with initialize

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,9 +3,9 @@ class Kele
   include HTTParty
 
   def initialize(email, password)
-    @base_uri = 'https://www.bloc.io/api/v1'
-    response = self.class.post('https://www.bloc.io/api/v1/sessions', { "email":email, "password":password} )
-    raise 'Invalid credentials' if response.code == 404
+    @base_url = 'https://www.bloc.io/api/v1'
+    response = self.class.post('sessions', body: { "email":email, "password":password} )
+    raise response.message
     @auth_token = response["auth_token"]
   end
 end
