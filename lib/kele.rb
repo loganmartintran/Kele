287b1d2501c4e2aa90d8c11d1569b7cc344b8184
Logan Tran <loganmartintran@icloud.com>
2017-07-06 18:33:46 -0500

    complete get_mentor_availability method

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,17 +3,29 @@ require 'json'
 
 class Kele
   include HTTParty
+  include JSON
 
   def initialize(email, password)
     @base_uri = 'https://www.bloc.io/api/v1'
     response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { "email":email, "password":password} )
+    raise response.message if response.code == 400
     puts response
-    raise response.message
     @auth_token = response["auth_token"]
   end
 
   def get_me
-    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+    # my id 2350193
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token } )
     @user = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+  # mentor id 2344341
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    availability = []
+    response.each do |time|
+      availability << time
+    end
+    availability
   end
 end
