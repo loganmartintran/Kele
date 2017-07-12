9aac1cf6c0d05b437630b485488bad6b74dbc86c
Logan Tran <loganmartintran@icloud.com>
2017-07-11 23:34:04 -0500

    add get_messages and create_message

--- lib/kele.rb
+++ lib/kele.rb
@@ -11,24 +11,39 @@ class Kele
     @base_uri = 'https://www.bloc.io/api/v1'
     response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { "email":email, "password":password} )
     raise response.message if response.code == 400
-    puts response
     @auth_token = response["auth_token"]
   end
 
   def get_me
     # my id 2350193
     response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token } )
-    @user = JSON.parse(response.body)
+    @me = JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
   # mentor id 2344341
-    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token } )
     availability = []
     response.each do |time|
-      availability << time
+      if time["booked"] == nil
+        availability << time
+      end
     end
     availability
   end
 
+  def get_messages
+    response = self.class.get('https://www.bloc.io/api/v1/message_threads', headers: { "authorization" => @auth_token } )
+    @messages = JSON.parse(response.body)
+  end
+
+  def create_message(sender_email, recipient_id, subject, stripped_text)
+    body = {
+      sender: sender_email,
+      recipient_id: recipient_id,
+      subject: subject,
+      stripped_text: stripped_text
+    }
+    response = self.class.post('https://www.bloc.io/api/v1/messages', headers: { "authorization" => @auth_token }, body: body)
+  end
 end
