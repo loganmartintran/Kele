1b524ae5b644ae0f970fa9d46acfc7918de79e1a
Logan Tran <loganmartintran@icloud.com>
2017-07-13 12:32:03 -0500

    fix get_messages method

--- lib/kele.rb
+++ lib/kele.rb
@@ -32,8 +32,12 @@ class Kele
     availability
   end
 
-  def get_messages
-    response = self.class.get('https://www.bloc.io/api/v1/message_threads', headers: { "authorization" => @auth_token } )
+  def get_messages(page='all')
+    if page == 'all'
+      response = self.class.get('https://www.bloc.io/api/v1/message_threads', headers: { "authorization" => @auth_token } )
+    else
+      response = self.class.get("https://www.bloc.io/api/v1/message_threads?page=#{page}", headers: { "authorization" => @auth_token } )
+    end
     @messages = JSON.parse(response.body)
   end
 
